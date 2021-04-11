7b1d4fe2f9b5b19384517317cf07fe1c8f6abfe8
Phillip Yuen <phillip@jomashop.io>
2021-04-11 13:33:35 -0400

    Ex 4.2
    add blog model
    add blogs router
    add utils

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,45 +1,18 @@
 require('dotenv').config()
 
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-const PORT = process.env.PORT
+const PORT = config.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    logger.info(`\r\nServer running on port ${PORT}`, `\r\nall systems operational`, `\r\nlasers set to death`)
 })
---
+++ bloglist/models/blog.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const logger = require("../utils/logger");
+const config = require("../utils/config");
+
+const url = config.MONGODB_URI;
+
+logger.info('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(result => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.info('error connecting to MongoDB:', error.message)
+    })
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    author: {
+        type: String,
+    },
+    url: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    likes: {
+        type: Number,
+    }
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
